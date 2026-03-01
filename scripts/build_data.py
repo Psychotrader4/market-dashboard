7e57372542af20ec9fd2d660bd640f2730ccad8d
Psychotrader4 <phagosom.mobil@googlemail.com>
2026-03-01 15:56:38 +0100

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -39,7 +39,7 @@ KEY_EVENTS = [
 
 # FIX 1: Duplikate entfernt (QQQ und RSP waren doppelt)
 STOCK_GROUPS = {
-    "Indices": ["SPY", "RSP", "QQQ", "QQQE", "GLD", "4GLD", "SLV", "ACWI", "SPYI", "VWCE", "VGWE", "VDIV"]
+    "Indices": ["SPY", "RSP", "QQQ", "QQQE", "GLD", "XETR:4GLD", "SLV", "XETR:ACWI", "XETR:SPYI", "XETR:VWCE", "XETR:VGWE", "XETR:VDIV"]
 }
 
 LEVERAGED_ETFS = {
