5e8e463f2d319abeeb31759337da3789a551c7f1
Psychotrader4 <phagosom.mobil@googlemail.com>
2026-03-01 15:01:40 +0100

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -39,7 +39,7 @@ KEY_EVENTS = [
 ]
 
 STOCK_GROUPS = {
-    "Indices, Metals, ETF": ["SPY", "RSP", "QQQ", "QQQE", "QQQ", "RSP", "GLD", "SLV", "ACWI", "SPYI.DE", "VWCE.DE", "VGWE.DE"]
+    "Indices": ["SPY", "RSP", "QQQ", "QQQE", "QQQ", "RSP", "GLD", "SLV", "ACWI", "SPYI.DE", "VWCE.DE", "VGWE.DE"]
 }
 
 LEVERAGED_ETFS = {
